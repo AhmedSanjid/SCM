23c713a9dd0bb81d410f92e2fffb79aee44bc532
AhmedSanjid <ahmedmukut19@gmail.com>
2024-10-14 14:54:46 +0600

    edited

--- database/migrations/2024_10_08_044846_create_insuranceclaims_table.php
+++ database/migrations/2024_10_08_044846_create_insuranceclaims_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name')->nullable();
             $table->string('company_name')->nullable();
             $table->string('product_name')->nullable();
-            $table->float('weight')->nullable(); // Assuming weight is a decimal value
+            $table->float('weight')->nullable();
             $table->string('destination')->nullable();
             $table->string('bank_name')->nullable();
             $table->string('cargo_serial_number')->nullable();
