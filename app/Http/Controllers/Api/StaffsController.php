af63a417c50a19eaf1cbc464cfa3cf1be83e0b1e
AhmedSanjid <ahmedmukut19@gmail.com>
2024-10-21 23:58:30 +0600

    migrations

--- app/Http/Controllers/Api/StaffsController.php
+++ app/Http/Controllers/Api/StaffsController.php
@@ -10,26 +10,26 @@ class StaffsController extends BaseController
 {
     public function index(){
         $data=Staff::get();
-        return $this->sendResponse($data,"Staffs data");
+        return $this->sendResponse($data,"Staff data");
     }
 
     public function store(Request $request){
         $data=Staff::create($request->all());
-        return $this->sendResponse($data,"Staffs created successfully");
+        return $this->sendResponse($data,"Staff created successfully");
     }
     public function show(Staff $staff){
-        return $this->sendResponse($staff,"Staffs created successfully");
+        return $this->sendResponse($staff,"Staff created successfully");
     }
 
     public function update(Request $request,$id){
 
         $data=Staff::where('id',$id)->update($request->all());
-        return $this->sendResponse($id,"Staffs updated successfully");
+        return $this->sendResponse($id,"Staff updated successfully");
     }
 
     public function destroy(Staff $staff)
     {
         $staff=$staff->delete();
-        return $this->sendResponse($staff,"Staffs deleted successfully");
+        return $this->sendResponse($staff,"Staff deleted successfully");
     }
 }
